21e2e81b47fb7262de9ac8415630146855f7d871
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-11-05 09:58:48 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan60-Akatsuki/src/if01/pkg10118016/latihan60/akatsuki/Akatsuki.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan60.akatsuki;
+
+/**
+ *
+ * @author Rezky
+ */
+public class Akatsuki {
+    protected String clothes;
+    protected String goals;
+
+    public Akatsuki(String clothes, String goals) {
+        this.clothes = clothes;
+        this.goals = goals;
+    }
+
+    public String getClothes() {
+        return clothes;
+    }
+
+    public String getGoals() {
+        return goals;
+    }
+  
+}
